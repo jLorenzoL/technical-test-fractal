4cf28e6c97b99ea658509adf7f7b4e340ebfb012
jlorenzo <jair.lorenzo.l@outlook.com>
2022-03-11 21:38:59 -0500

    Creating component

---
+++ src/main/java/com/fractal/demo/technicaltestfractal/TechnicalTestFractalApplication.java
@@ -0,0 +1,13 @@
+package com.fractal.demo.technicaltestfractal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalTestFractalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalTestFractalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fractal/demo/technicaltestfractal/TechnicalTestFractalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fractal.demo.technicaltestfractal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalTestFractalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
